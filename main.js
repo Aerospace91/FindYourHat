1253184590ac66f519e853e4d74471a4616b040f
Cory Burmahl <beenters@gmail.com>
2024-07-31 08:48:47 -0700

    Basic Win/Loss handling

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const hat = "^";
 const hole = "O";
 const fieldCharacter = "░";
 const pathCharacter = "*";
-let gameState = false
+let gameState = true
 
 
 class Field {
@@ -21,7 +21,40 @@ class Field {
     console.log(newStr);
   }
   makeMove(move){
-    return this.findIndex('*')
+    let [x, y] = this.findIndex('*')
+    const x2 = x
+    const y2 = y
+    //Adjust Coordinates
+    switch (move) {
+        case 'u':
+            x -= 1
+            break;
+        case 'd':
+            x += 1
+            break;
+        case 'l':
+            y -= 1
+            break;
+        case 'r':
+            y += 1
+            break;
+        default:
+            return "Invalid Move"
+    }
+    // Check for "Out of Bounds"
+    if (x < 0 || x >= this._field.length || y < 0 || y >= this._field[x2].length) {
+        console.log("Out of Bounds")
+        return 0
+    }
+
+    // Move the Character
+    if (this._field[x][y] === hole) {
+        return 0
+    } else if (this._field[x][y] === hat) {
+        return 1
+    }
+    this._field[x2][y2] = fieldCharacter
+    this._field[x][y] = pathCharacter
   }
   findIndex(char){
     for (let i = 0; i < this._field.length; i++){
@@ -40,13 +73,21 @@ const myField = new Field([
     ["░", "^", "░"],
   ]);
 
-while (!gameState){
+while (gameState){
     myField.print();
     const input = prompt("Make your move (u, d, l, r, exit): ")
     if (input === 'exit'){
-        gameState = true
+        gameState = false
+        break;
     }else if (['u', 'd', 'l', 'r'].includes(input)){
-        console.log(myField.makeMove(input))
+        const move = myField.makeMove(input)
+        if (move === 0){
+            console.log("You Lost!")
+            gameState = false
+        } else if (move === 1) {
+            console.log("You Won!")
+            gameState = false
+        }
     }else{
         console.log('Invalid Input')
     }
